9f1a162c118e16ff36b91802b203b355f2728e48
stampnoon <thnong_stamp@hotmail.com>
2020-03-19 14:41:16 +0700

    add new picture

--- bot.php
+++ bot.php
@@ -2725,7 +2725,7 @@ if (!is_null($events)) {
                     $replyData = new ImagemapMessageBuilder(
                         $imageWebsite,
                         'test',
-                        new BaseSizeBuilder(800, 1200),
+                        new BaseSizeBuilder(1200, 1200),
                         array(
                             new ImagemapMessageActionBuilder(
                                 'คำถาม:เว็บ1',

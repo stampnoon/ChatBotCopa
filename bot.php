91241c3df8928e991500491ba0b6a9d521909e36
stampnoon <thnong_stamp@hotmail.com>
2020-03-25 12:32:23 +0700

    test input image

--- bot.php
+++ bot.php
@@ -2979,8 +2979,8 @@ if (!is_null($events)) {
                 //     );
                 // } 
                 else if ($userMessage == "test") {
-                    $replyData = new TextMessageBuilder('asdasdasd');
-                    $replyData = json_encode($replyData);
+                    $test = new TextMessageBuilder('asdasdasd');
+                    $replyData = new TextMessageBuilder(json_encode($test));
                 }
                 // else if ($userMessage == "push") {
                 //     $responseProfile = $bot->getProfile(LINE_USER_ID);

28d559c9069e603568d906642c7456e0d44bb42a
BosSaksit <saksit.kku@gmail.com>
2020-04-23 16:49:23 +0700

    test multimessage

--- bot_test.php
+++ bot_test.php
@@ -3454,7 +3454,26 @@ if (!is_null($events)) {
                                         )
                                     )
                                 )
-                            ) // end bubble 10    
+                            ), // end bubble 10  
+                            new BubbleContainerBuilder(
+                                "ltr",  // กำหนด NULL หรือ "ltr" หรือ "rtl"
+                                NULL,NULL,
+                                new BoxComponentBuilder(
+                                    "horizontal",
+                                    array(
+                                        new TextComponentBuilder("Hello, World 11",NULL,NULL,NULL,NULL,NULL,true)
+                                    )
+                                ),
+                                new BoxComponentBuilder(
+                                    "horizontal",
+                                    array(
+                                        new ButtonComponentBuilder(
+                                            new UriTemplateActionBuilder("GO","http://niik.in"),
+                                            NULL,NULL,NULL,"primary"
+                                        )
+                                    )
+                                )
+                            ) // end bubble 11      
                         )
                     );
 
